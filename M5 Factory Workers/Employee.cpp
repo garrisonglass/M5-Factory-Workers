dd77bb7c35f69d3f08c39a60331aa19ad3897359
garrisonglass <garrison.glass@gmail.com>
2026-04-13 21:25:08 -0400

    Added ProductionWorker.h

---
+++ M5 Factory Workers/Employee.cpp
@@ -0,0 +1,28 @@
+#include "Employee.h"
+#include <iostream>
+
+using namespace std;
+
+Employee::Employee() : name(""), EmployeeID(0), startingDate("") {}
+
+//Overloaded constructor
+Employee::Employee(const string& name, int EmployeeID, const string& startingDate) 
+	: name(name), EmployeeID(EmployeeID), startingDate(startingDate) {}
+
+//Accessors
+string Employee::getName() const { return name; }
+int Employee::getEmployeeID() const { return EmployeeID; }
+string Employee::getStartingDate() const { return startingDate; }
+
+//Mustators
+void Employee::setName(const string& name) { this->name = name; }
+void Employee::setEmployeeID(int EmployeeID) { this->EmployeeID = EmployeeID; }
+void Employee::setStartingDate(const string& startingDate) { this->startingDate = startingDate; }
+
+//Print function
+void Employee::printEmployee() const
+{
+	cout << "Employee Name: " << name         << endl;
+	cout << "Employee ID: "   << EmployeeID   << endl;
+	cout << "Starting Date: " << startingDate << endl;
+}
---
+++ M5 Factory Workers/ProductionWorker.h
@@ -0,0 +1,23 @@
+#ifndef PRODUCTIONWORKER_H
+#define PRODUCTIONWORKER_H
+
+#include "Employee.h"
+
+class ProductionWorker : public Employee	
+{
+	private:
+		int shift; //1 for day shift, 2 for night shift
+		double payRate;
+
+public:
+	ProductionWorker();
+	ProductionWorker(const string& name, int EmployeeID, const string& startingDate, int shift, double payRate);
+
+	void setShift(int shift);
+	void setPayRate(double payRate);
+
+	int getShift() const;
+	double getPayRate() const;
+};
+
+#endif

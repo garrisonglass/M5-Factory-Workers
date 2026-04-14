43c83d4eee00724843b96adbb7012e68be6329a8
garrisonglass <garrison.glass@gmail.com>
2026-04-13 21:34:49 -0400

    Added Shif Supervisor.h

---
+++ M5 Factory Workers/ShiftSupe.cpp
@@ -0,0 +1,1 @@
+#include "ShiftSupe.h"
---
+++ M5 Factory Workers/ShiftSupe.h
@@ -0,0 +1,29 @@
+#ifndef SHIFTSUPERVISOR_H
+#define SHIFTSUPERVISOR_H
+
+#include "Employee.h"
+
+class ShiftSupe : public Employee
+{
+	double salary;
+	double bonus;
+
+public:
+
+	//Default constructor
+	ShiftSupe();
+	ShiftSupe(string name, int EmployeeID, string startingDate, double salary, double bonus);
+
+	//Mutators
+	void setSalary(double salary);
+	void setBonus(double bonus);
+
+	//Accessors
+	double getSalary() const;
+	double getBonus() const;
+
+	//Print function
+	void printShiftSupe() const;
+};
+
+#endif

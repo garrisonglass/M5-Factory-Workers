e7f548214c76a986e67dcbef98614d6c7e6796ac
garrisonglass <garrison.glass@gmail.com>
2026-04-13 21:46:17 -0400

    Added Shift Suoervisor . cpp

--- M5 Factory Workers/ShiftSupe.cpp
+++ M5 Factory Workers/ShiftSupe.cpp
@@ -1,1 +1,34 @@
 #include "ShiftSupe.h"
+#include <iostream>
+
+using namespace std;
+
+//Default constructor
+ShiftSupe::ShiftSupe() : Employee() 
+{
+	salary = 0.0;
+	bonus = 0.0;
+}
+
+//Overloaded constructor
+ShiftSupe::ShiftSupe(string name, int EmployeeID, string startingDate, double salary, double bonus)
+	: Employee(name, EmployeeID, startingDate)
+{
+	this->salary = salary;
+	this->bonus = bonus;
+}
+
+void ShiftSupe::setSalary(double salary) { this->salary = salary; }
+
+void ShiftSupe::setBonus(double bonus) { this->bonus = bonus; }
+
+double ShiftSupe::getSalary() const { return salary; }
+
+double ShiftSupe::getBonus() const { return bonus; }
+
+void ShiftSupe::printShiftSupe() const
+{
+	printEmployee();
+	cout << "Salary: " << salary << endl;
+	cout << "Bonus: " << bonus << endl;
+}

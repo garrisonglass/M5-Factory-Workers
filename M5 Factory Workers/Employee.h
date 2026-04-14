00636e6c13fab68c80939892221c09eca8808fdd
garrisonglass <garrison.glass@gmail.com>
2026-04-13 21:05:08 -0400

    Add project files.

---
+++ M5 Factory Workers/Employee.h
@@ -0,0 +1,32 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string>
+using namespace std;
+class Employee
+{
+private:
+	string name;
+	int EmployeeID;
+	string startingDate;
+
+public:
+	//Constructor
+	Employee();
+	Employee(const string& time, int EmployeeID, const string& startingDate);
+
+	//Accessors
+	string getName() const;
+	int getEmployeeID() const;
+	string getStartingDate() const;
+
+	//Mutators
+	void setName(const string& name);
+	void setEmployeeID(int EmployeeID);
+	void setStartingDate(const string& startingDate);
+
+	//Print function
+	void printEmployee() const;
+};
+
+#endif//EMPLOYEE_H

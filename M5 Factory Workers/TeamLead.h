e1265dc07d527bb878b10585039d4c174e101269
garrisonglass <garrison.glass@gmail.com>
2026-04-14 17:42:40 -0400

    Added Team Lead.h

--- M5 Factory Workers/TeamLead.h
+++ M5 Factory Workers/TeamLead.h
@@ -1,5 +1,30 @@
 #ifndef TEAMLEADER_H
 #define TEAMLEADER_H
 
+#include "ProductionWorker.h"
+
+class TeamLead : public ProductionWorker
+{
+
+private:
+	double monthlyBonus;
+	int requiredTrainingHours;
+	int attendedTrainingHours;
+
+public:
+	//Constructor	
+	TeamLead();
+	TeamLead(const string& name, int EmployeeID, const string& startingDate, int shift, double payRate, double monthlyBonus, int requiredTrainingHours, int attendedTrainingHours);
+	//Accessors	
+	double getMonthlyBonus() const;
+	int setRequiredTrainingHours() const;
+	int setAttendedTrainingHours() const;
+	//Mutators
+	void setMonthlyBonus(double bonus);
+	void getRequiredTrainingHours(int hours);
+	void getAttendedTrainingHours(int hours);
+	//Print function
+	void printTeamLead() const;
+};
 
 #endif

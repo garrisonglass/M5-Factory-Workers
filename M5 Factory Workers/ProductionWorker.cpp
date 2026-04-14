8ffde9b45957c81651f0d692d0e067e08c8054a5
garrisonglass <garrison.glass@gmail.com>
2026-04-13 21:29:50 -0400

    Added ProductionWorker.cpp

--- M5 Factory Workers/ProductionWorker.cpp
+++ M5 Factory Workers/ProductionWorker.cpp
@@ -1,1 +1,22 @@
 #include "ProductionWorker.h"
+
+//Default constructor
+ProductionWorker::ProductionWorker() : Employee() 
+{
+	shift = 1;
+	payRate = 0.0;
+}
+
+//Overloaded constructor
+ProductionWorker::ProductionWorker(const string& name, int EmployeeID, const string& startingDate, int shift, double payRate) 
+	: Employee(name, EmployeeID, startingDate), shift(shift), payRate(payRate) {
+}
+
+void ProductionWorker::setShift(int shift) { this->shift = shift; }
+
+void ProductionWorker::setPayRate(double payRate) { this->payRate = payRate; }
+
+int ProductionWorker::getShift() const { return shift; }
+
+double ProductionWorker::getPayRate() const { return payRate; }	
+
